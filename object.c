dcb59ed0c4352d789a725f7b7dc6008ec8ab2be3
Harsimran Kaur <harsimran.kaur2271@gmail.com>
2026-04-23 20:20:54 +0530

    Implement object_read with header parsing
    Implement object_read function to read object data from file

--- object.c
+++ object.c
@@ -102,6 +102,38 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
 }
 
 int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_t *len_out) {
-    (void)id; (void)type_out; (void)data_out; (void)len_out;
-    return -1;
+    char path[512];
+    object_path(id, path, sizeof(path));
+
+    FILE *f = fopen(path, "rb");
+    if (!f) return -1;
+
+    fseek(f, 0, SEEK_END);
+    size_t size = ftell(f);
+    rewind(f);
+
+    void *buffer = malloc(size);
+    if (!buffer) { fclose(f); return -1; }
+
+    fread(buffer, 1, size, f);
+    fclose(f);
+
+    char *nul = memchr(buffer, '\0', size);
+    if (!nul) { free(buffer); return -1; }
+
+    char type[10];
+    size_t data_len;
+
+    sscanf(buffer, "%s %zu", type, &data_len);
+
+    if (strcmp(type, "blob") == 0) *type_out = OBJ_BLOB;
+    else if (strcmp(type, "tree") == 0) *type_out = OBJ_TREE;
+    else *type_out = OBJ_COMMIT;
+
+    *data_out = malloc(data_len);
+    memcpy(*data_out, nul + 1, data_len);
+    *len_out = data_len;
+
+    free(buffer);
+    return 0;
 }
